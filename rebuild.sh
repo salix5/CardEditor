837b75d2191c361449a90c16040b7aba9366b042
salix5 <salix5566@gmail.com>
2026-03-20 00:28:54 +0800

    Reduce connection timeout to 10

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl --ssl-revoke-best-effort --retry 2 --connect-timeout 30 -O https://salix5.github.io/archive/ygopro.tar.gz
-curl --ssl-revoke-best-effort --retry 2 --connect-timeout 30 -L -O https://github.com/salix5/windbot/releases/latest/download/WindBot.tar.gz
+curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -O https://salix5.github.io/archive/ygopro.tar.gz
+curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/windbot/releases/latest/download/WindBot.tar.gz
 tar -xzf ygopro.tar.gz
 tar -xzf WindBot.tar.gz
 rm WindBot.tar.gz
