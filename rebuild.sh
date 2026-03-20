5b7a727e116594cd9ca8905c690ddc4b385e10d3
salix5 <salix5566@gmail.com>
2026-03-20 23:44:04 +0800

    Add -L flag to curl commands

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -O https://salix5.github.io/archive/ygopro.tar.gz
+curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://salix5.github.io/archive/ygopro.tar.gz
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/windbot/releases/latest/download/WindBot.tar.gz
 tar -xzf ygopro.tar.gz
 tar -xzf WindBot.tar.gz
--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 --create-dirs --output-dir "expansions" -O https://cdn.jsdelivr.net/gh/salix5/ypk@master/pre-release.ypk
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/cdb/releases/latest/download/cards.cdb
-curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -O https://cdn.jsdelivr.net/gh/salix5/CardEditor@master/strings.conf
-curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -O https://cdn.jsdelivr.net/gh/salix5/CardEditor@master/lflist.conf
-curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -O https://cdn.jsdelivr.net/gh/salix5/CardEditor@master/rebuild.sh
+curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://cdn.jsdelivr.net/gh/salix5/CardEditor@master/strings.conf
+curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://cdn.jsdelivr.net/gh/salix5/CardEditor@master/lflist.conf
+curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://cdn.jsdelivr.net/gh/salix5/CardEditor@master/rebuild.sh
 git clone https://github.com/salix5/ygopro-pack.git pack
 git clone https://github.com/Fluorohydride/ygopro-scripts.git script
 cd script
