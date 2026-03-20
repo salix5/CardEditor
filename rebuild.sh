7c2078c14b45247ddc01fa13d5485e6697b63627
salix5 <salix5566@gmail.com>
2026-03-21 01:25:46 +0800

    add rm

--- rebuild.sh
+++ rebuild.sh
@@ -3,6 +3,7 @@ curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://salix
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/windbot/releases/latest/download/WindBot.tar.gz
 tar -xzf ygopro.tar.gz
 tar -xzf WindBot.tar.gz
+rm ygopro.tar.gz
 rm WindBot.tar.gz
 mkdir deck
 mkdir replay
