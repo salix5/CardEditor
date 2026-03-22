0d81749ddc4362e8785790ded53af1c72bd95f4e
salix5 <salix5566@gmail.com>
2026-03-22 21:54:58 +0800

    improve directory handling in update.sh

--- update.sh
+++ update.sh
@@ -4,11 +4,20 @@ curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://githu
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/CardEditor/releases/latest/download/strings.conf
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/CardEditor/releases/latest/download/lflist.conf
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/CardEditor/releases/latest/download/rebuild.sh
-git clone https://github.com/salix5/ygopro-pack.git pack
-git clone https://github.com/Fluorohydride/ygopro-scripts.git script
-cd script
-git checkout master
-git branch -D test
-git pull
-cd ../pack
-git pull
+if [ -d "pack" ]; then
+    cd pack
+    git pull
+    cd ..
+else
+    git clone https://github.com/salix5/ygopro-pack.git pack
+fi
+
+if [ -d "script" ]; then
+    cd script
+    git checkout master
+    git branch -D test 2>/dev/null
+    git pull
+    cd ..
+else
+    git clone https://github.com/Fluorohydride/ygopro-scripts.git script
+fi
