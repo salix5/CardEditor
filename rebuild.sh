fe08042fc4deb251afbc3033695993753ca22f60
salix5 <salix5566@gmail.com>
2026-03-22 18:37:55 +0800

    Update download links in rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://salix5.github.io/archive/ygopro.tar.gz
+curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/ygopro/releases/latest/download/YGOPro.exe
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/windbot/releases/latest/download/WindBot.tar.gz
-tar -xzf ygopro.tar.gz
 tar -xzf WindBot.tar.gz
-rm ygopro.tar.gz
 rm WindBot.tar.gz
 mkdir deck
 mkdir replay
