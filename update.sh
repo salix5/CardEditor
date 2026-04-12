ec14ce82fe5684b054354aece3871c8260926a5f
salix5 <salix5566@gmail.com>
2026-04-12 17:13:25 +0800

    update ypk URL and improve git commands

--- update.sh
+++ update.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 --create-dirs --output-dir "expansions" -L -O https://cdn.jsdelivr.net/gh/salix5/ypk@master/pre-release.ypk
+curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 --create-dirs --output-dir "expansions" -L -O https://github.com/salix5/ypk/releases/latest/download/pre-release.ypk
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/cdb/releases/latest/download/cards.cdb
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/CardEditor/releases/latest/download/strings.conf
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/CardEditor/releases/latest/download/lflist.conf
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/CardEditor/releases/latest/download/rebuild.sh
-if [ -d "pack" ]; then
-    cd pack
-    git pull
-    cd ..
+if [ -d "pack/.git" ]; then
+    git -C pack pull
 else
+    rm -rf pack
     git clone https://github.com/salix5/ygopro-pack.git pack
 fi
 
-if [ -d "script" ]; then
-    cd script
-    git checkout master
-    git branch -D test 2>/dev/null
-    git pull
-    cd ..
+if [ -d "script/.git" ]; then
+    git -C script checkout master
+    git -C script pull
 else
+    rm -rf script
     git clone https://github.com/Fluorohydride/ygopro-scripts.git script
 fi
