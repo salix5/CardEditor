4f11513dbc9df4d270fb396f5b97ebd35ee6f39c
salix5 <salix5566@gmail.com>
2026-05-11 01:37:18 +0800

    rebuild: use version variable

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/ygopro/releases/latest/download/YGOPro.exe
+VERSION="v1.5.0"
+curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O "https://github.com/salix5/ygopro/releases/download/${VERSION}/YGOPro.exe"
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/windbot/releases/latest/download/WindBot.tar.gz
 tar -xzf WindBot.tar.gz
 rm WindBot.tar.gz
