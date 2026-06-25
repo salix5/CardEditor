c6825281947e9b60e5dd90562b60dd6055bc745e
salix5 <salix5566@gmail.com>
2026-06-26 00:18:03 +0800

    download packages.tar.gz in update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 --create-dirs --output-dir "expansions" -L -O https://github.com/salix5/ypk/releases/latest/download/pre-release.ypk
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/cdb/releases/latest/download/cards.cdb
-curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/CardEditor/releases/latest/download/strings.conf
-curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/CardEditor/releases/latest/download/lflist.conf
-curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/CardEditor/releases/latest/download/rebuild.sh
+curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/CardEditor/releases/latest/download/packages.tar.gz
+tar -xzf packages.tar.gz
+rm packages.tar.gz
 if [ -d "pack/.git" ]; then
     git -C pack pull
 else
