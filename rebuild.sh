7631e6f7f7a3a83735dd3f2637d09350dda91f6a
salix5 <salix5566@gmail.com>
2026-06-27 23:36:20 +0800

    Refactor rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-VERSION="v1.7.0"
-curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O "https://github.com/salix5/ygopro/releases/download/${VERSION}/YGOPro.exe"
+VERSION="v1.7.2"
+curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O "https://github.com/salix5/ygopro/releases/download/${VERSION}/YGOPro-${VERSION}-x64.tar.gz"
+tar -xzf "YGOPro-${VERSION}-x64.tar.gz"
+rm "YGOPro-${VERSION}-x64.tar.gz"
 curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/windbot/releases/latest/download/WindBot.tar.gz
 tar -xzf WindBot.tar.gz
 rm WindBot.tar.gz
-mkdir deck
-mkdir replay
-mkdir single
+curl --ssl-revoke-best-effort --retry 2 --connect-timeout 10 --create-dirs --output-dir "textures" -L -O "https://github.com/salix5/ygopro/releases/latest/download/lim_credit.png"
+mkdir -p deck
+mkdir -p replay
+mkdir -p single
