72ab139ff20939c447ec3d10e10882a7e4b51a67
salix5 <salix5566@gmail.com>
2026-08-01 17:11:16 +0800

    Download single script in update.sh

--- update.sh
+++ update.sh
@@ -2,8 +2,11 @@
 curl -q --fail --ssl-revoke-best-effort --retry 2 --connect-timeout 10 --create-dirs --output-dir "expansions" -L -O https://github.com/salix5/ypk/releases/latest/download/pre-release.ypk
 curl -q --fail --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/cdb/releases/latest/download/cards.cdb
 curl -q --fail --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/CardEditor/releases/latest/download/packages.tar.gz
+curl -q --fail --ssl-revoke-best-effort --retry 2 --connect-timeout 10 -L -O https://github.com/salix5/CardEditor/releases/latest/download/single.tar.gz
 tar -xzf packages.tar.gz
 rm packages.tar.gz
+tar -xzf single.tar.gz
+rm single.tar.gz
 if [ -d "pack/.git" ]; then
     git -C pack pull
 else
